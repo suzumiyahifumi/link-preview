f3d32d0283c452a7db264cbb0b73c3b0d18014f3
Liquid Sausage 100% <37862699+suzumiyahifumi@users.noreply.github.com>
2023-06-30 02:29:28 +0800

    cors_2

--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,8 @@ export default {
     const reqUrl = new URL(request.url);
     const origin = request.headers.get("Origin") || "*";
     const referer = request.headers.get("Referer");
-    if(referer != undefined){
-      if (reqUrl.hostname != new URL('https://trpgtime.2d6exp.com/').hostname || new URL(referer).hostname != new URL('https://ttrpg.2d6exp.com/').hostname) {
+    if(referer){
+      if (reqUrl.hostname != new URL('https://trpgtime.2d6exp.com/').hostname && new URL(referer).hostname != new URL('https://ttrpg.2d6exp.com/').hostname) {
           return handleError(500, `[Failed by cors]`, origin);;
       }
     }
